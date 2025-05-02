a810c36f69ff1654e330146ddc771e48d2573a31
FreddyPinto <freddypinto@outlook.com>
2025-05-01 21:28:11 -0500

    Implement session-based navigation and add LogoutForm component

--- frontend/src/app/private/page.js
+++ frontend/src/app/private/page.js
@@ -1,9 +1,15 @@
-import React from 'react'
-
-const Private = () => {
+import { getSession } from "@/actions";
+import { redirect } from "next/navigation";
+const page = async () => {
+  const session = await getSession();
+  if (!session?.jwt) {
+    redirect("/login");
+  }
   return (
-    <div>Private</div>
-  )
-}
-
-export default Private
+    <div className="p-4">
+      <h1>Private Page</h1>
+      <pre>{JSON.stringify(session, null, 2)}</pre>
+    </div>
+  );
+};
+export default page;
