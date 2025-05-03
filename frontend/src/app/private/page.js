c834a15db6137d9cdd1df1175676c72cdcb71eaf
FreddyPinto <freddypinto@outlook.com>
2025-05-02 19:02:16 -0500

    Add CarForm and InputField components for car data submission

--- frontend/src/app/private/page.js
+++ frontend/src/app/private/page.js
@@ -1,14 +1,18 @@
+import CarForm from "@/components/CarForm";
 import { getSession } from "@/actions";
 import { redirect } from "next/navigation";
+
 const page = async () => {
   const session = await getSession();
+
   if (!session?.jwt) {
     redirect("/login");
   }
+
   return (
     <div className="p-4">
       <h1>Private Page</h1>
-      <pre>{JSON.stringify(session, null, 2)}</pre>
+      <CarForm />
     </div>
   );
 };
