1680a37bf8ed978d230dea17fb115695468f1466
FreddyPinto <freddypinto@outlook.com>
2025-05-03 21:33:53 -0500

    Providing metadata for SEO

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,12 +1,17 @@
 import "./globals.css";
 import Navbar from "@/components/NavBar";
 
+export const metadata = {
+  title: "Farm Cars App",
+  description: "Next.js + FastAPI + MongoDB App",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         <Navbar />
-        {children}
+        <div className="p-4  min-h-[calc(100vh-56px)]">{children}</div>
       </body>
     </html>
   );
